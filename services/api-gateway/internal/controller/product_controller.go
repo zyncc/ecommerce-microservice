1325238c0a1aaa37dc2ff558520354dc83aa1848
zyncc <chandankrishna288@gmail.com>
2026-06-29 17:32:37 +0530

    update docker compose

--- services/api-gateway/internal/controller/product_controller.go
+++ services/api-gateway/internal/controller/product_controller.go
@@ -22,6 +22,16 @@ func NewProductController(log *zap.Logger, productClient *client.ProductClient) 
 	}
 }
 
+// CreateProduct godoc
+// @Summary Create Product
+// @Description Creates a new Product
+// @Tags Product
+// @Accept json
+// @Produce json
+// @Param request body dto.CreateProductRequest true "Create Product Request"
+// @Success 200 {object} uuid.UUID
+// @Failure 500 {object} utils.Error
+// @Router /api/v1/product [post]
 func (c *ProductController) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var req dto.CreateProductRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -37,6 +47,15 @@ func (c *ProductController) CreateProduct(w http.ResponseWriter, r *http.Request
 	utils.SuccessResponse(w, http.StatusOK, "Product Created", &id)
 }
 
+// GetAllProducts godoc
+// @Summary Fetch all products
+// @Description Fetch all products
+// @Tags Product
+// @Accept json
+// @Produce json
+// @Success 200 {object} []dto.Product
+// @Failure 500 {object} utils.Error
+// @Router /api/v1/product [get]
 func (c *ProductController) GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	products, err := c.productClient.GetAllProducts(r.Context())
 	if err != nil {
